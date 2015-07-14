124cfba6d558677205e61ca28ab27f7668d6710f
Kangsik Kevin Lee <kangsik.kevin.lee@gmail.com>
2015-07-14 21:54:54 +0000

    post Crud-module created by yo

--- app/models/post.server.model.js
+++ app/models/post.server.model.js
@@ -10,19 +10,61 @@ var mongoose = require('mongoose'),
  * Post Schema
  */
 var PostSchema = new Schema({
-	name: {
+	title: {
 		type: String,
 		default: '',
-		required: 'Please fill Post name',
+		required: 'Please fill Post title',
+		trim: true
+	},
+	content: {
+		type: String,
+		default: '',
+		required: 'Please fill Post content',
+		trim: true
+	},
+	category: {
+		type: String,
+		default: '',
+		required: 'Please choose Post category',
+		trim: true
+	},
+	topic: {
+		type: String,
+		default: '',
+		required: 'Please choose Post topic',
 		trim: true
 	},
 	created: {
 		type: Date,
 		default: Date.now
 	},
+	updated: {
+		type: Date,
+		default: Date.now
+	},
+	postBy: {
+		type: String,
+		default: '',
+		required: 'please identify author',
+		trim: true
+	},
 	user: {
 		type: Schema.ObjectId,
 		ref: 'User'
+	},
+	
+	likes: {
+		type: String,
+		default: '0',
+		trim: true
+	},
+	comments: {
+		type: String,
+		default: '0',
+		trim: true
+	},
+	hot: {
+		type: Boolean
 	}
 });
 
