2bb8e0f845c378c079d37a0f22e542396f9de267
Kangsik Kevin Lee <kangsik.kevin.lee@gmail.com>
2015-07-15 00:00:10 +0000

    new post successfully loaded to post list ha ha

--- public/modules/core/controllers/home.client.controller.js
+++ public/modules/core/controllers/home.client.controller.js
@@ -1,9 +1,9 @@
 'use strict';
 
 
-angular.module('core').controller('HomeController', ['$scope', 'Authentication', '$resource',
-	function($scope, Authentication, $resource) {
-		//var Post = $resource('/api/posts');
+angular.module('core').controller('HomeController', ['$scope', 'Authentication', 
+	function($scope, Authentication) {
+	
 		
 		// This provides Authentication context.
 		$scope.authentication = Authentication;
--- public/modules/posts/controllers/posts.client.controller.js
+++ public/modules/posts/controllers/posts.client.controller.js
@@ -4,7 +4,62 @@
 angular.module('posts').controller('PostsController', ['$scope', '$stateParams', '$location', 'Authentication', 'Posts',
 	function($scope, $stateParams, $location, Authentication, Posts) {
 		$scope.authentication = Authentication;
-
+		$scope.currentCategoryIndex = 0;
+		$scope.currentCategory = 'Share Your Ideas';
+		$scope.postTopic = 'Business Strategy';
+		$scope.categories = [
+			
+			{
+				id: 'introduce-yourself',
+				title: 'Introduce Yourself',
+				snippet: 'Get to know other cashew members',
+				topics: [
+					{ name: 'Friends',
+						numPosts: 0,
+						numNewPosts:0,
+					}
+					]
+			},
+			{
+				id: 'share-your-ideas',
+				title: 'Share Your Ideas',
+				snippet: 'Share ideas, interests, strategies',
+				topics: [
+					{ name: 'Business Strategy',
+						numPosts: 4,
+						numNewPosts:4,
+					},
+					{ name: 'Custommer Strategy',
+						numPosts: 1,
+						numNewPosts: 1,
+					},
+					{ name: 'Other Topics',
+						numPosts: 2,
+						numNewPosts: 2,
+					}
+				]
+			},
+			{
+				id: 'feedback-for-cashew',
+				title: 'Feedback For Cashew',
+				snippet: 'We likes to hear from you!',
+				topics: [
+					{ name: 'Q&A',
+						numPosts: 4,
+						numNewPosts:4,
+					},
+					{ name: 'feedBack',
+						numPosts: 1,
+						numNewPosts: 1,
+					},
+					{ name: 'Other Topics',
+						numPosts: 2,
+						numNewPosts: 2,
+					}
+				]
+			}
+		];
+		
 		// Create new Post
 		$scope.create = function() {
 			// Create new Post object
@@ -12,7 +67,8 @@ angular.module('posts').controller('PostsController', ['$scope', '$stateParams',
 				title: this.title,
 				content: this.content,
 				category: this.category,
-				topic: this.topic
+				topic: this.topic,
+				postBy: this.postBy
 			});
 
 			// Redirect after save
@@ -24,6 +80,7 @@ angular.module('posts').controller('PostsController', ['$scope', '$stateParams',
 				$scope.content = '';
 				$scope.category = '';
 				$scope.topic = '';
+				$scope.postBy = '';
 			}, function(errorResponse) {
 				$scope.error = errorResponse.data.message;
 			});
