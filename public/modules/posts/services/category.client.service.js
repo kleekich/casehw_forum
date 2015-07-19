cb4409613111015dc0b445457ff076cecac9c2db
Kangsik Kevin Lee <kangsik.kevin.lee@gmail.com>
2015-07-19 10:43:54 +0000

    category service added

---
+++ public/modules/posts/services/category.client.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('posts').factory('Category', [
+	function() {
+		// Category service logic
+		// ...
+
+		// Public API
+		return {
+			someMethod: function() {
+				return true;
+			}
+		};
+	}
+]);
