aff0f46655c6420716ee0afc4063fc1690459af0
Kangsik Kevin Lee <kangsik.kevin.lee@gmail.com>
2015-07-22 22:38:49 +0000

    comment model added by yo generator

---
+++ app/models/comment.server.model.js
@@ -0,0 +1,53 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Comment Schema
+ */
+var CommentSchema = new Schema({
+	commentTo: {
+		type: String,
+		default: '',
+		requried: 'postID requried',
+		trim: true
+	},
+	
+	comment: {
+		type: String,
+		default: '',
+		required: 'Please fill comment content',
+		trim: true
+	},
+	created: {
+		type: Date,
+		default: Date.now
+	},
+	updated: {
+		type: Date,
+		default: Date.now
+	},
+	commentBy: {
+		type: String,
+		default: '',
+		//required: 'please identify author',
+		trim: true
+	},
+	user: {
+		type: Schema.ObjectId,
+		ref: 'User'
+	},
+	
+	likes: {
+		type: String,
+		default: '0',
+		trim: true
+	},
+
+});
+
+mongoose.model('Comment', CommentSchema);
